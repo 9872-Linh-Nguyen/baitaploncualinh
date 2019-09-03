cf3ea3a5217dff24613983aec06b5cb7c8f28bf4
fe3o4tmh <tmh.hung.97@gmail.com>
2019-09-03 13:49:03 +0700

    init

---
+++ app/src/main/java/com/linhnguyen/baitaploncualinh/UI/HomePage/MainActivity.java
@@ -0,0 +1,36 @@
+package com.linhnguyen.baitaploncualinh.UI.HomePage;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.linhnguyen.baitaploncualinh.R;
+import com.linhnguyen.baitaploncualinh.UI.Profile.Profile;
+
+public class MainActivity extends AppCompatActivity implements Contact.View {
+    //Widget
+    private ImageButton img_profile;
+    //Intent
+    private Intent intent;
+
+    private void init() {
+        img_profile = findViewById(R.id.img_profile);
+        img_profile.setOnClickListener(view -> {
+            intent = new Intent(this, Profile.class);
+            startActivity(intent);
+        });
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    @Override
+    public void onBackPressed() {
+    }
+}

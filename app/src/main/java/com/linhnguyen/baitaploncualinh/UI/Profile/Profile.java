29aa732973ebe39fcf35b24f9cb9deb9f21173aa
fe3o4tmh <tmh.hung.97@gmail.com>
2019-09-03 14:15:12 +0700

    init profile layout

--- app/src/main/java/com/linhnguyen/baitaploncualinh/UI/Profile/Profile.java
+++ app/src/main/java/com/linhnguyen/baitaploncualinh/UI/Profile/Profile.java
@@ -2,15 +2,28 @@ package com.linhnguyen.baitaploncualinh.UI.Profile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.linhnguyen.baitaploncualinh.R;
+import com.linhnguyen.baitaploncualinh.UI.HomePage.MainActivity;
 
 public class Profile extends AppCompatActivity {
+    private Intent intent;
+    private void init(){
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
     }
 }
